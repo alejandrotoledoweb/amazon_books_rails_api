1fb6cf248ff76c80fe7dfc7e24272962781c9e7d
Alejandro Toledo <atoledofr@gmail.com>
2023-06-01 16:27:58 -0500

    added the delete method to the books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,11 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    Book.find(params[:id]).destroy!
+    head :no_content
+  end
+
   private
 
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :books, only: [:index, :create]
+  resources :books, only: [:index, :create, :destroy]
 
 end
